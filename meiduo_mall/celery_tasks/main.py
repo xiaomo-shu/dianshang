7f1b7a5e92ed3e0e1932f6172a32b603129937fa
smartlz <1009098051@qq.com>
2018-07-04 12:50:27 +0800

    校验用户名和手机号是否重复接口

--- meiduo_mall/celery_tasks/main.py
+++ meiduo_mall/celery_tasks/main.py
@@ -1,5 +1,11 @@
 from celery import Celery
 
+# 为celery使用django配置文件进行设置
+import os
+if not os.getenv('DJANGO_SETTINGS_MODULE'):
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'meiduo_mall.settings.dev'
+
+
 # 创建一个Celery对象
 # celery_app = Celery('celery_tasks', broker='中间人地址')
 celery_app = Celery('celery_tasks')
