a8468d1829ce2cb32ccd086671a7ccc2bfc6d589
smartlz <1009098051@qq.com>
2018-07-05 10:57:22 +0800

    浏览器本地存储JWT_Token信息

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -221,6 +221,13 @@ var vm = new Vue({
                         responseType: 'json'
                     })
                     .then(response => {
+                        // 保存签发的jwt token
+                        // 记录用户的登录状态
+                        sessionStorage.clear();
+                        localStorage.clear();
+                        localStorage.token = response.data.token;
+                        localStorage.username = response.data.username;
+                        localStorage.user_id = response.data.id;
                         location.href = '/index.html';
                     })
                     .catch(error=> {
