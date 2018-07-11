a17a541d3dc6b3e521d565424be7c0f10591e779
smartlz <1009098051@qq.com>
2018-07-11 09:53:54 +0800

    获取分类商品(所有)列表数据后端API

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
+from .serializers import SKUSerializer
+from .models import SKU
 # Create your views here.
+
+
+# GET /categories/(?P<category_id>\d+)/skus/?page=xxx&page_size=xxx&ordering=xxx
+class SKUListView(ListAPIView):
+    serializer_class = SKUSerializer
+
+    def get_queryset(self):
+        # 获取分类id
+        category_id = self.kwargs['category_id']
+
+        return SKU.objects.filter(category_id=category_id, is_launched=True)
