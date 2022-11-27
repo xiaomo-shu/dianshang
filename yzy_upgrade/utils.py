c59023e8445386f50bbac6cd1cc86545a3e37a75
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-27 06:07:21 +0000

    Adding tarfile member sanitization to extractall()

--- yzy_upgrade/utils.py
+++ yzy_upgrade/utils.py
@@ -207,7 +207,26 @@ def decompress_package(package_path):
 
     try:
         with tarfile.open(package_path, "r") as fd:
-            fd.extractall(constants.UPGRADE_TMP_PATH)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(fd, constants.UPGRADE_TMP_PATH)
         logger.info("Extract upgrade package to %s successful.", constants.UPGRADE_TMP_PATH)
         return True
     except Exception:
